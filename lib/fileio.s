a704433bd00fc69b1387534d9a03c0cb00502583
Alex  Morte <alexandersonbm@hotmail.com>
2022-09-27 19:40:07 -0300

    feat: add fileio

---
+++ lib/fileio.s
@@ -0,0 +1,16 @@
+.equ sys_open, 5
+
+@ File IO
+@ This module opens a file. 
+@ Its used to open /dev/mem
+
+.macro open_file file
+    LDR R0, =\file
+    LDR R1, =mode
+    LDR R1, [R1]
+    MOV R7, #sys_open
+    SVC 0
+.endm
+
+.data
+mode: .word 2
