09a24314814ce2066a7d116f2cd5dd16e26336a4
Alex  Morte <alexandersonbm@hotmail.com>
2022-09-28 00:02:06 -0300

    feat: update example project

--- examples/countdown.c
+++ examples/countdown.c
@@ -1,16 +1,14 @@
 #include <stdio.h>
 
 extern void init();
-extern void writea();
+extern void write_char(char ch);
 extern void clear_display();
 
 int main(){
     init();
-    printf("initilized\n");
-
-    for(int i=0;i<10;i++) writea();
-    clear_display();
-    writea();
-    printf("Writed\n");
+    printf("Initialized\n");
+    char str[] = "iniciando";
+    for(int i=0;i<21;i++) write_char(str[i]);
+    printf("Written\n");
     return 0;
 }
