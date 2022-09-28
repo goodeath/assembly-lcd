983dccbda8fed7fe198b4f3250dcbf97120b6553
Alex  Morte <alexandersonbm@hotmail.com>
2022-09-28 16:53:48 -0300

    refactor: nanosleep

--- lib/utils.s
+++ lib/utils.s
@@ -1,10 +1,13 @@
 .section .text
-
+.equ sys_nanosleep, #162
+@ Nanosleep
+@ seconds - Time in Seconds
+@ nano - Time in nanoseconds
 .macro nanosleep seconds nano
-    PUSH {R0-R6}
-    ldr r0, =\seconds
-    ldr r1, =\nano
-    mov r7, #162
-    svc 0
-    POP {R0-R6}
+    PUSH {R0-R1}
+    LDR r0, =\seconds
+    LDR r1, =\nano
+    MOV r7, #sys_nanosleep
+    SVC 0
+    POP {R0-R1}
 .endm
